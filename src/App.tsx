83420fec05e04c627d386f7b92757c74e6e38115
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-09-07 12:38:03 +0200

    Install storybook, styled components & typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import dogFacts from './data.json'
+
+export default function App() {
+  return (
+    <div className="App">
+     {/* {dogFacts.map(dogFact => {
+        const { fact, id } = dogFact
+        return (
+          <div key={id}>{fact}</div>
+            
+            
+            
+         
+        )
+      })} */}
+    </div>
+  )
+}
